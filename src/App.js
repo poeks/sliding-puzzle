a6b55e06528b96cc79fc1b227c7989618c3c626e
Roel <roelwinter11@gmail.com>
2021-05-09 15:25:37 +0200

    Add initial react code

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>Sliding puzzle</div>
+      </header>
+      <div>Gameboard</div>
+    </div>
+  );
+}
+
+export default App;
